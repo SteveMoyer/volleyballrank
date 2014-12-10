38cad5c8bd50bfb4eb98bc9c5111e786fca59f24
Steve Moyer <smoyer@google.com>
2014-12-10 08:36:43 -0800

    add execute permissions on depswriter

--- runclosure.sh
+++ runclosure.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+chmod +x src/main/webapp/bower_components/closure-libraray/closure/bin/build/depswriter.py
 src/main/webapp/bower_components/closure-libraray/closure/bin/build/depswriter.py --root_with_prefix="src/main/webapp/app ../../../../app" --output_file=src/main/webapp/deps.js
